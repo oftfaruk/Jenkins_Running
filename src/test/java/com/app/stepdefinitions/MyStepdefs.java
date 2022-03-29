475b897e78ba22509c623fe249a4bd46e5e614d0
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-29 16:03:56 +0100

    headless1

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.sql.SQLException;
 
 public class MyStepdefs {
@@ -21,11 +22,11 @@ public class MyStepdefs {
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-       // Bypass OS security model
+        // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage");
-       // System.setProperty("webdriver.chrome.driver", "http://18.203.176.202:8080");
+        // System.setProperty("webdriver.chrome.driver", "http://18.203.176.202:8080");
         //options.setBinary("http://18.203.176.202:8080");
-
+        options.addArguments("--remote-debugging-port=9222");
 
 
         ChromeDriver driver = new ChromeDriver(options);
