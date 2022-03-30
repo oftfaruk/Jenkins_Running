8131369fd094fcc60991d55a9aa7ed3143a49e95
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-30 09:36:11 +0100

    delete

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -1,6 +1,5 @@
 package com.app.stepdefinitions;
 
-import com.app.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -12,32 +11,32 @@ import java.sql.SQLException;
 public class MyStepdefs {
     @Given("User is on the login page")
     public void userIsOnTheLoginPage() throws SQLException {
-
+    //    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        System.setProperty("webdriver.chrome.args", "--disable-logging");
-        System.setProperty("webdriver.chrome.silentOutput", "true");
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        // Bypass OS security model
-        options.addArguments("--disable-dev-shm-usage");
-        // System.setProperty("webdriver.chrome.driver", "http://18.203.176.202:8080");
-        //options.setBinary("http://18.203.176.202:8080");
-        options.addArguments("--remote-debugging-port=9222");
-        String chromeDriverPath = "./usr/bin/chromedriver";
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-
-
-        ChromeDriver driver1 = new ChromeDriver(options);
-        driver.get(ConfigurationReader.get("url"));
+//        ChromeOptions options = new ChromeOptions();
+//
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        System.setProperty("webdriver.chrome.args", "--disable-logging");
+//        System.setProperty("webdriver.chrome.silentOutput", "true");
+//        options.setExperimentalOption("useAutomationExtension", false);
+//        options.addArguments("--disable-gpu"); // applicable to windows os only
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("disable-infobars"); // disabling infobars
+//        options.addArguments("--disable-extensions"); // disabling extensions
+//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//        // Bypass OS security model
+//        options.addArguments("--disable-dev-shm-usage");
+//        // System.setProperty("webdriver.chrome.driver", "http://18.203.176.202:8080");
+//        //options.setBinary("http://18.203.176.202:8080");
+//        options.addArguments("--remote-debugging-port=9222");
+//        String chromeDriverPath = "./usr/bin/chromedriver";
+//        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+//
+//
+//        ChromeDriver driver1 = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
 
 
     }
