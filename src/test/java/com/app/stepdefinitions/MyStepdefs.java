d9d7ea4ee6ce5806fb1f350103cb1a0c9ed0ce03
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-31 10:50:15 +0100

    delete

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -12,53 +12,17 @@ import java.util.Collections;
 public class MyStepdefs {
     @Given("User is on the login page")
     public void userIsOnTheLoginPage() throws SQLException {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-     //   System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "\\usr\\bin\\chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.setBinary("/usr/bin/google-chrome");
-        options.addArguments("--no-sandbox");
-        options.addArguments("headless");
-        WebDriver driver = new ChromeDriver(options);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-setuid-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-
-//        options.setExperimentalOption("useAutomationExtension", false);
-//
-//        WebDriverManager.chromedriver().setup();
-//
-//
-//       // options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");  //chrome binary location specified here
-//        options.addArguments("start-maximized");
-//        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-//        options.setExperimentalOption("useAutomationExtension", false);
-//        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.google.com/");
-
-
-//
-
-//        options.addArguments("--headless");
-//        System.setProperty("webdriver.chrome.args", "--disable-logging");
-//        System.setProperty("webdriver.chrome.silentOutput", "true");
-//        options.setExperimentalOption("useAutomationExtension", false);
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//
-//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        // Bypass OS security model
-//        options.addArguments("--disable-dev-shm-usage");
-//        // System.setProperty("webdriver.chrome.driver", "http://18.203.176.202:8080");
-//        //options.setBinary("http://18.203.176.202:8080");
-//        options.addArguments("--remote-debugging-port=9222");
-//        String chromeDriverPath = "./usr/bin/chromedriver";
-//        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-//
-//
-//        ChromeDriver driver1 = new ChromeDriver(options);
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://google.com");
 
 
     }
