988b636b31e49b2769eeac2c8138ade705c20cda
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-31 11:05:04 +0100

    delete

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -12,17 +12,20 @@ import java.util.Collections;
 public class MyStepdefs {
     @Given("User is on the login page")
     public void userIsOnTheLoginPage() throws SQLException {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-
+//        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox"); // Bypass OS security model
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("disable-infobars"); // disabling infobars
+//        options.addArguments("--disable-extensions"); // disabling extensions
+//        options.addArguments("--disable-gpu"); // applicable to windows os only
+//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver(options);
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver();
+
         driver.get("https://google.com");
 
 
