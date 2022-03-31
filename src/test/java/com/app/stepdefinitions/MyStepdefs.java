9bc14142d024bbdec18220ce137101d394572625
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-31 15:21:56 +0100

    delete

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class MyStepdefs {
     @Given("User is on the login page")
     public void userIsOnTheLoginPage() {
-     //   System.setProperty("webdriver.chrome.driver", "");
+        //   System.setProperty("webdriver.chrome.driver", "");
 //        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--no-sandbox"); // Bypass OS security model
 //        options.addArguments("start-maximized"); // open Browser in maximized mode
@@ -19,7 +19,7 @@ public class MyStepdefs {
 //        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
 //        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//options.add_argument('--remote-debugging-port=9222')
+
 //        WebDriverManager.chromedriver().setup();
 ////        WebDriver driver = new ChromeDriver(options);
 
@@ -28,6 +28,7 @@ public class MyStepdefs {
 //        options.addArguments("--no-sandbox");
 //        options.addArguments("--disable-dev-shm-usage");
         //   WebDriverManager.chromedriver().setup();
+        options.addArguments("--remote-debugging-port=9222");
         options.addArguments("headless");
 //        WebDriver driver = new ChromeDriver();
         WebDriver driver = new ChromeDriver(options);
