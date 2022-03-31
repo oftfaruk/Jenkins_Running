b5bbac5aab5c710d6652001528538ce943593729
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-31 16:02:59 +0100

    delete

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -11,7 +11,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class MyStepdefs {
     @Given("User is on the login page")
     public void userIsOnTheLoginPage() {
-       System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+      // System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+       System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
 //        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--no-sandbox"); // Bypass OS security model
 //        options.addArguments("start-maximized"); // open Browser in maximized mode
@@ -33,7 +34,7 @@ public class MyStepdefs {
 
         options.addArguments("headless");
 //        WebDriver driver = new ChromeDriver();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://google.com");
 
 
