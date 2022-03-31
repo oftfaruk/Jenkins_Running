79fcc76a8b117af59bb1edc5c476f86b1aea8520
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-03-31 14:52:59 +0100

    delete

---
+++ src/test/java/com/app/runnners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.app.runnners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/htmlreprt.html",
+               // "rerun:target/rerun.txt"
+                },
+        features = "src/test/resources/features/Web",
+        glue =  "com/app/stepdefinitions",
+        strict = true,
+        dryRun = false,
+        tags = "@smoke11"
+)
+public class CukesRunner {
+
+}
