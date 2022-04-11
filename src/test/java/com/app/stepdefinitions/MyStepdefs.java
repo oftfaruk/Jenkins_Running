dcd79c19edca80685a0cf973a77eb9c6b334f3b6
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-11 15:03:14 +0100

    driver_changes

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -70,11 +70,11 @@ public class MyStepdefs {
         options.addArguments("headless");
         options.setBinary("/usr/bin/google-chrome");
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.google.com/");
+        driver.get("https://test.rack-eye.com/login");
         String title = driver.getTitle();
-      //  String text = driver.findElement(By.xpath("//label[@for='username']")).getText();
+        String text = driver.findElement(By.xpath("//label[@for='username']")).getText();
         System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
-     //   System.out.println("text " + text);
+        System.out.println("text " + text);
 
         System.out.println("hello  deneme123");
     }
