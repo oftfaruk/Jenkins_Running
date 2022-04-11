ea17e19f9ad77a504bd85eeecaa446a4964b5012
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-11 16:39:41 +0100

    driver_changes

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -73,7 +73,7 @@ public class MyStepdefs {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-        options.setBinary("/usr/bin/google-chrome");
+      //  options.setBinary("/usr/bin/google-chrome");
 
         driver = new ChromeDriver(options);
 //        driver.get("https://google.com");
