ddb3b68ca39ea8c135e7f06ff961ebbd907cd1e5
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-13 11:13:56 +0100

    driver_changes

--- src/test/java/com/app/utilities/Driver.java
+++ src/test/java/com/app/utilities/Driver.java
@@ -42,22 +42,18 @@ public class Driver {
                     break;
                 case "chrome-headless":
                     WebDriverManager.chromedriver().setup();
-                    ChromeOptions chOption= new ChromeOptions();
-
-                 //   driverPool.set(new ChromeDriver(new ChromeOptions().setHeadless(true)));
-                    chOption.addArguments("--no-sandbox"); // Bypass OS security model
-                    chOption.addArguments("disable-gpu");
-//                    chOption.setHeadless(true);
+                    ChromeOptions chOption = new ChromeOptions();
                     chOption.addArguments("disable-infobars");
                     chOption.addArguments("--disable-extensions");
-                    chOption.addArguments("--disable-dev-shm-usage");
-//                    chOption.addArguments("--disable-notifications");
-//                    chOption.addArguments("--disable-web-security");
-//                    chOption.addArguments("--no-proxy-server");
-//                    chOption.addArguments("--enable-automation");
-                    //   chOption.addArguments("--disable-save-password-bubble");
-                    //  chOption.addArguments("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/91.0.4472.114 Safari/537.36");
-                       driverPool.set(new ChromeDriver(chOption));
+                    chOption.addArguments("--disable-notifications");
+                    chOption.addArguments("--start-maximized");
+                    chOption.addArguments("--disable-web-security");
+                    chOption.addArguments("--no-proxy-server");
+                    chOption.addArguments("--enable-automation");
+                    chOption.addArguments("--disable-save-password-bubble");
+                    chOption.setHeadless(true);
+                    driverPool.set(new ChromeDriver(new ChromeOptions()));
+                    driverPool.set(new ChromeDriver(chOption.setHeadless(true)));
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
