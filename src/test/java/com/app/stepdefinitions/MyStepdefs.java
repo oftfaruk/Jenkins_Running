81079edfe25570cf733b31daf35d61d89ea32a48
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-16 07:11:17 +0100

    gecko

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -201,7 +201,7 @@ public class MyStepdefs {
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new FirefoxDriver(options);
         driver.get("https://google.com");
 
 
