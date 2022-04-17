c41fed4d24e66f6d4318f5dd3ad2ed8cb27daff0
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-17 10:00:42 +0100

    port

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -225,7 +225,7 @@ public class MyStepdefs {
 //        WebDriver driver = new FirefoxDriver(opt);
 
 //        o.add_argument('--remote-debugging-port=9222')
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver","/tmp");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-debugging-port=9222");
