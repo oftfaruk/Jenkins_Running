3ae1ed7682866607073e7f46c62f11319f6d79a3
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-17 11:27:10 +0100

    port

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -228,6 +228,17 @@ public class MyStepdefs {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--test-type");
+        options.addArguments("test-type");
+        options.addArguments("start-maximized");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--enable-precise-memory-info");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-default-apps");
+        options.addArguments("test-type=browser");
+        options.addArguments("--incognito");
+        options.addArguments("--no-sandbox");
         //options.setBinary("/tmp");
         options.addArguments("--remote-debugging-port=9222");
         options.addArguments("--no-sandbox");
