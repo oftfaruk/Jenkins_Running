9a6f15e0600a6f38ffa84fb59da91cc80b24bca9
oftfaruk <71329710+oftfaruk@users.noreply.github.com>
2022-04-18 12:24:37 +0100

    bin.firefox

--- src/test/java/com/app/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/app/stepdefinitions/MyStepdefs.java
@@ -256,13 +256,13 @@ public class MyStepdefs {
 ////        driver.get("https://www.google.com/");
 
 
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-         options.setBinary("/usr/bin/google-chrome");
+      //  options.addArguments("--no-sandbox");
+       //  options.setBinary("/usr/bin/google-chrome");
         options.addArguments("--headless");
-        options.addArguments("--disable-dev-shm-usage");
+      //  options.addArguments("--disable-dev-shm-usage");
       //  options.addArguments("--remote-debugging-port=9222");
         WebDriver driver = new ChromeDriver(options);
        // driver.get("https://www.google.com/");
